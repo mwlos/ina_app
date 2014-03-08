8fb6b36d727fb970ec261324746e5241ed4ca927
mwlos <kentro_molos@hotmail.com>
2014-03-08 19:18:21 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InaApp::Application.config.secret_key_base = '4920fd1765b85f5353d7224b5035fcc9343f610aed419722e11f5b70c9a96d97ea5b6c5617df5b57e31c6f6c147331d1548e7775968bb8b7cbded72dc3b99225'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+InaApp::Application.config.session_store :cookie_store, key: '_ina_app_session'
